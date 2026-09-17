17bb438a35282bfb86782d92e64ae5f431590335
Yagiz Pekcan <yagiz.pekcan@smu.ca>
2026-09-17 11:44:30 -0300

    Add braces so else matches intended if

--- src/Trace3.java
+++ src/Trace3.java
@@ -3,12 +3,12 @@ public class Trace3 {
     public static void main(String[] args) {
         int score = 45;
 
-        if (score > 90)
+        if (score > 90) {
             if (score > 95)
                 System.out.println("A+");
-        else
+        }  else {
             System.out.println("Not an A");
-
+        }
         System.out.println("done");
     }
 }
